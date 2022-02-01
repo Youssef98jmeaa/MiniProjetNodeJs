b3da8047de9937434c0e9d50fc38f907b8c1cd8a
youssef jmeaa <jmeaayoussef98@gmail.com>
2022-02-02 00:52:51 +0100

    atlas database

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/uploads',express.static('uploads'))
 
 //app.use('/users/lot/search',UsersRouter)
 
-app.listen(80, () => {
+app.listen(5000, () => {
 	console.log("Server is running on port " );
 })
 //app.listen(3000, ()=> console.log('server start'))
